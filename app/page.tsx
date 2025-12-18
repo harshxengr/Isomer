2e03f3aac40a1ead3ddeb0aa6c7b535e6bc9b6f6
Harsh Saini <singhharshsaini7@gmail.com>
2025-12-18 20:43:03 +0530

    Added database

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { prisma } from "@/lib/db"
 
-export default function Home() {
+export default async function Home() {
 
   let something = true;
+  const users = await prisma.user.findMany();
 
   return (
     <div className={cn("text-red-500 font-bold", something === true && "bg-green-500")}>
       harsh
-      <Button>harsh </Button>
+      <Button>harsh</Button>
+      {users.map((user: any) => (
+        <div key={user.id}>{user.name}</div>
+      ))}
     </div>
   );
 }
