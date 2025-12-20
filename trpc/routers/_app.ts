b676ec4add2ab9b9c4104192cf2165e6131ac024
Harsh Saini <singhharshsaini7@gmail.com>
2025-12-20 23:39:11 +0530

    Authentication done

--- trpc/routers/_app.ts
+++ trpc/routers/_app.ts
@@ -1,11 +1,14 @@
 // import { z } from 'zod';
 import prisma from '@/lib/db';
-import { baseProcedure, createTRPCRouter } from '../init';
+import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
 export const appRouter = createTRPCRouter({
-  getUsers: baseProcedure
-    .query(() => {
-      return prisma.user.findMany();
-    }),
+  getUsers: protectedProcedure.query(({ ctx }) => {
+    return prisma.user.findMany({
+      where: {
+        id: Number(ctx.auth.user.id),
+      }
+    });
+  }),
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
